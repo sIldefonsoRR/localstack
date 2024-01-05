4b100a3df7d7ee55a61b1a9c21d4ca67c883336c
Sérgio Ildefonso <s.ildefonso@runtime-revolution.com>
2024-01-05 18:07:53 +0000

    :sparkles: Added EC2 instance

--- main.tf
+++ main.tf
@@ -77,3 +77,14 @@ resource "aws_s3_bucket" "example" {
     Environment = "Dev"
   }
 }
+
+
+/* EC2 */
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "JacksBlogExample"
+  }
+}
